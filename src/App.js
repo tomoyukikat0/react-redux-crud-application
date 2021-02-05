d571732a25695d659606cceb1af9503c1d1bac2b
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-02-06 03:18:47 +0900

    update firestore

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
+import PropTypes from 'prop-types'
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName", age: 100}
+  ]
   return (
-    <div>Hi!</div>
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
   )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
