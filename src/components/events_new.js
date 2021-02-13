2de3a72bea36426d28d3b757b174e88582834e2c
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-02-13 22:53:50 +0900

    add disable status

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -28,7 +28,7 @@ class EventsNew extends Component {
   }
 
   render () {
-    const { handleSubmit } = this.props
+    const { handleSubmit, pristine, submitting } = this.props
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -38,7 +38,7 @@ class EventsNew extends Component {
         </div>
 
         <div>
-          <input type="submit" value="Submit" disabled={false} />
+          <input type="submit" value="Submit" disabled={pristine || submitting} />
           <Link to="/">Cancel</Link>
         </div>
       </form>
